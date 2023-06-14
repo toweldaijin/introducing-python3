50f5f2e16f9742155f0d0eaf8b9d45a6a01b0d13
toweldaijin <colourfulflower21@gmail.com>
2023-06-14 23:44:47 +0900

    add

---
+++ chap3.py
@@ -0,0 +1,19 @@
+3.1
+print(60 ** 2);
+
+3.2
+seconds_per_hour = 60 ** 2;
+print(seconds_per_hour);
+
+3.3
+print(seconds_per_hour * 24);
+
+3.4
+seconds_per_day = seconds_per_hour * 24;
+print(seconds_per_day);
+
+3.5
+print(seconds_per_day / seconds_per_hour);
+
+3.6
+print(seconds_per_day // seconds_per_hour);
