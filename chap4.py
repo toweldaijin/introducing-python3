b39241e4ec98b6ae563eab74d8a716e4d49411ce
toweldaijin <colourfulflower21@gmail.com>
2023-06-15 22:43:31 +0900

    solved

---
+++ chap4.py
@@ -0,0 +1,28 @@
+
+#4.1
+secret = 3
+guess = 8
+
+if secret > guess:
+    print("too low")
+elif secret < guess:
+    print("too high")
+else:
+    print("just right")
+
+#4.2
+small = True
+green = False
+if small:
+    if green:
+        print("pea")
+    else:
+        print("cherry")
+else:
+    if green:
+        print("wetermelon")
+    else:
+        print("pumpkin")
+
+
+
